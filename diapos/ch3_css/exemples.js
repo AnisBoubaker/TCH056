8b4988d38dfeec4d752a72874ca870f908458dd0
Anis Boubaker <anis@boubaker.ca>
2024-01-11 15:58:57 -0500

    Grid Layout et définition du responsive design.

--- diapos/ch3_css/exemples.js
+++ diapos/ch3_css/exemples.js
@@ -478,7 +478,348 @@ exemples = [
         }],
         links: [],
         selected: 'app.css'
-    }
+    },
+
+    { //Exemple 11: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template-rows: 5rem 20rem 5rem;
+    grid-template-columns: 20% 80%;
+    /* Ou avec une ligne grid-template: */
+    /*grid-template: 5rem 20rem 5rem / 20% 80%; */
+}
+.boite{
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}
+
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <div class="boite">A</div>
+    <div class="boite">B</div>
+    <div class="boite">C</div>
+    <div class="boite">D</div>
+    <div class="boite">E</div>
+    <div class="boite">F</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    },
+
+    { //Exemple 12: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template: 5rem 20rem 5rem / 20% 80%; 
+    
+    /* Notez que la valeur par défaut ici c'est stretch, ce qui grossit
+    l'élément pour atteindre la taille de la cellule (sauf si l'élément a
+    une taille déjà définie.) */
+    justify-items: center;
+    align-items: end;
+}
+.boite{
+    height: 3rem;
+    width: 3rem;
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+    
+    /* Il est aussi possible de le faire l'alignement directement dans 
+    la cellule concernée avec ces deux déclarations */
+    /*justify-self: center;
+    align-self: center;*/
+}
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <div class="boite">A</div>
+    <div class="boite">B</div>
+    <div class="boite">C</div>
+    <div class="boite">D</div>
+    <div class="boite">E</div>
+    <div class="boite">F</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    } ,
+
+    { //Exemple 13: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    border: 1px solid lightcoral;
+    display: grid;
+    height: 35rem;
+    grid-template: 5rem 20rem 5rem / 20% 40%; 
+    
+    justify-items: center;
+    align-items: end;
+
+    /* Utilisez l'inspecteur pour afficher l'overlay de la grille 
+    pour bien apprécier ce qui se passe */
+    justify-content: center;
+    align-content: top;
+}
+.boite{
+    height: 3rem;
+    width: 3rem;
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <div class="boite">A</div>
+    <div class="boite">B</div>
+    <div class="boite">C</div>
+    <div class="boite">D</div>
+    <div class="boite">E</div>
+    <div class="boite">F</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    },
+
+    { //Exemple 14: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    width: 20rem;
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template-rows: 5rem 20rem 5rem;
+    /* 
+    Constatez que la grille dépasse les limtes du conteneur
+    (qui a un bord couleur corail)
+    Remplacez les % par des fr et observez
+    le résultat*/
+    grid-template-columns: 100px 20% 80%; 
+}
+.boite{
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}    
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <div class="boite">A</div>
+    <div class="boite">B</div>
+    <div class="boite">C</div>
+    <div class="boite">D</div>
+    <div class="boite">E</div>
+    <div class="boite">F</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    } ,
+
+    { //Exemple 15: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    width: 20rem;
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template-rows: 5rem 20rem 5rem;
+    grid-template-columns: 100px 20fr 80fr; 
+}
+.boite{
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}    
+
+.boite-a{
+    grid-column: 2 / 4;
+    grid-row: 1;
+}
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <div class="boite boite-a">A</div>
+    <div class="boite">B</div>
+    <div class="boite">C</div>
+    <div class="boite">D</div>
+    <div class="boite">E</div>
+    <div class="boite">F</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    } ,
+
+    { //Exemple 16: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    height: 100vh;
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template-rows: 4rem auto 4rem;
+    grid-template-columns: 100px 80fr 20fr; 
+    grid-template-areas: 
+        "entete entete    entete"
+        "menu   principal connexe"
+        "bas    bas       connexe"
+}
+.zone-page{
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}    
+
+.entete{
+    grid-area: entete;
+}
+
+.navigation{
+    grid-area: menu;
+}
+
+.connexe{
+    grid-area: connexe;
+}
+
+.pied-page{
+    grid-area: bas;
+}
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <header class="zone-page entete">En-tête</header>
+    <nav class="zone-page navigation">Menu du site</nav>
+    <main class="zone-page principal">Section principale du site</main>
+    <aside class="zone-page connexe">Liens et informations connexes à la page</aside>
+    <div class="zone-page pied-page">Pied de page</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    },
+
+    { //Exemple 17: 
+        files: [ {
+            name: 'app.css',
+            editable : true,
+            content: `
+.conteneur{
+    height: 100vh;
+    border: 1px solid lightcoral;
+    display: grid;
+    grid-template-rows: 4rem auto 4rem;
+    grid-template-columns: 100px 80fr 20fr; 
+    grid-template-areas: 
+        "entete entete    entete"
+        "menu   principal connexe"
+        "bas    bas       connexe"
+}
+.zone-page{
+    background-color: gold;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid black;
+}    
+
+.entete{
+    grid-area: entete;
+    display: flex;
+    justify-content: space-between;
+    align-items: center
+}
+
+.logo{
+    align-self: start;
+    margin-left: 1rem;
+}
+
+.login_links{
+    align-self: start;
+    margin-right: 1rem;
+}
+
+.navigation{
+    grid-area: menu;
+}
+
+.connexe{
+    grid-area: connexe;
+}
+
+.pied-page{
+    grid-area: bas;
+}
+            `
+        }, {
+            name: 'app.html',
+            content: `
+<div class="conteneur">
+    <header class="zone-page entete">
+        <div class='logo'>Logo</div>
+        <div>Titre du site</div>
+        <div class='login_links'>Login</div>
+    </header>
+    <nav class="zone-page navigation">Menu du site</nav>
+    <main class="zone-page principal">Section principale du site</main>
+    <aside class="zone-page connexe">Liens et informations connexes à la page</aside>
+    <div class="zone-page pied-page">Pied de page</div>
+</div>
+            `
+        }],
+        links: [],
+        selected: 'app.css'
+    }  
+
+
 
 
 ]
